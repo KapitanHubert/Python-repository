0eeaf9c54e60cecbaa0924f5929c71fc65a2df29
Hubert Kapitan <kapitan.hubert98@gmail.com>
2026-05-19 15:01:34 +0200

    add project that allow to download html script into folder /websites

---
+++ BasicProjects/html downloader/main.py
@@ -0,0 +1,41 @@
+import os, sys
+import urllib.parse
+import validators # pip instal validators
+import requests
+from datetime import datetime
+print("Number of arguments :",len(sys.argv))
+print("ArgumentsList :",sys.argv)
+url = "https://duckduckgo.com"
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+print("website to download",url)
+
+scriptDir = os.path.dirname(__file__)
+os.chdir(scriptDir)
+print("Curent worki Dir:", os.getcwd())
+
+if not os.path.exists("./websites"):
+    os.mkdir("websites")
+
+parsedUrl = urllib.parse.urlparse(url)
+print(parsedUrl)
+
+validFlag = validators.url(url)
+if validFlag:
+    print("Url: ",url,"is valid")
+else:
+    print("Url: ",url,"is invalid")
+    raise Exception("Bad URL!")
+
+response = requests.get(url, allow_redirects = True)
+if response.ok == True:
+    print("Responsse ok from server for url:", url)
+    now = datetime.now()
+    dateString = now.strftime("%d.%m.%Y %H.%M.%S")
+    print(dateString)
+    fileName = "./websites/" + parsedUrl.netloc + " " + dateString + ".html"
+    print(fileName)
+    fh = open(fileName , "wb")
+    fh.write(response.content)
+    fh.close()
+
